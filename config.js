c4d08f10d15c8d1ad568f184bed15ceed0e70bd5
A38696 <75258993+A38696@users.noreply.github.com>
2021-09-11 00:14:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Tiến <33',
     btnNo: 'Không,bạn Tiến à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Tiến <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu Anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Mãi yêu em không yêu anh bắn vỡ sọ kkk>>>',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
